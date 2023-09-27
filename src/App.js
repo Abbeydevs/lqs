689fc93711154b3f66fdb275225f591fe040f300
abbeydevs <abbey.developer@gmail.com>
2023-09-27 23:47:25 +0100

    setup --> pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Contact from "./pages/contact/Contact";
+import Homepage from "./pages/home/Home";
+import Review from "./pages/review/Review";
+import Services from "./pages/services/Services";
+import Team from "./pages/team/Team";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <HashRouter>
+        <Routes>
+          <Route path="*" element="404 page" />
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/the-team" element={<Team />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
---
+++ src/pages/team/Team.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Team() {
+  return <div>Team Page</div>;
+}
+
+export default Team;
